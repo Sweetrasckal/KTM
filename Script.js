dd62532d1be6143f95d073077243aed190fe6b87
Sweetrasckal <52298280+Sweetrasckal@users.noreply.github.com>
2020-08-18 19:38:00 +0530

    'JS'

--- Script.js
+++ Script.js
@@ -2,6 +2,14 @@ var database =[
     {
         username: "Hari",
         password: "Secret"
+    },
+    {
+        username: "Krish",
+        password: "12345"
+    },
+    {
+        username: "Messi",
+        password: "leftfoot"
     }
 ];
 
@@ -19,7 +27,31 @@ var newsFeed =[
 var userNamePrompt = prompt("Enter Username");
 var passwordPrompt = prompt("Enter Password");
 
-function login(name, pass){
+function validUser(name,pass)
+{
+    for(var i=0; i<database.length; i++)
+    {
+        if(database[i].username === name && database[i].password === pass)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+function login(name,pass)
+{
+    if(validUser(name,pass))
+    {
+        console.log(newsFeed);
+    }
+    else
+    {
+        console.log("Invalid Creds");
+    }
+}
+
+/* function login(name,pass){
     if(name === database[0].username && pass === database[0].password)
     {
         console.log(newsFeed);
@@ -28,6 +60,6 @@ function login(name, pass){
     {
         console.log("Improper Details");
     }
-}
+} */
 
-login(userNamePrompt, passwordPrompt);
+login(userNamePrompt,passwordPrompt);
